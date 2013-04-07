bfe5a3b46779f21585db3fc02fe45fa9a6c4a48b
Gui Iribarren <gui@altermundi.net>
2013-04-07 20:51:50 +0200

    replace hardcodings with s/ar71xx/$TARGET/g

--- set_environment.sh
+++ set_environment.sh
@@ -15,8 +15,8 @@ scripts/feeds update -a
 scripts/feeds install -a
 
 echo "Copying config"
-cp -f configs/ar71xx/config .config || error config for this target does not exist
+cp -f configs/$TARGET/config .config || error config for this target does not exist
 
 echo "Copying files"
 [ ! -e files ] && mkdir files
-[ -d configs/$TARGET/files ] && cp -rf configs/ar71xx/files/* files/ 2>/dev/null
+[ -d configs/$TARGET/files ] && cp -rf configs/$TARGET/files/* files/ 2>/dev/null
