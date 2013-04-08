567f912fa0a8a2928fe769779c698109af1b5483
Gui Iribarren <gui@altermundi.net>
2013-04-08 15:12:29 +0200

    use scripts/env for managing environments

--- set_environment.sh
+++ set_environment.sh
@@ -8,15 +8,8 @@ error() {
 
 TARGET=${1-ar71xx}
 
-[ ! -d configs/$TARGET ] && error target does not exist
-
 echo "Updating feeds"
 scripts/feeds update -a
 scripts/feeds install -a
 
-echo "Copying config"
-cp -f configs/$TARGET/config .config || error config for this target does not exist
-
-echo "Copying files"
-[ ! -e files ] && mkdir files
-[ -d configs/$TARGET/files ] && cp -rf configs/$TARGET/files/* files/ 2>/dev/null
+scripts/env switch $TARGET || error environment for this target does not exist
