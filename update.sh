5e3154e54f1050b291c662c4340d5941315abc43
p4u <pau@dabax.net>
2013-07-29 17:45:46 +0200

    Small improvement in update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 LOCAL_SERVER="192.168.1.1:/www/wbm"
 REMOTE_SERVER="fw.qmp.cat:/var/www/firmware/wbm6"
-ENVS="tl-wdr mr3040 x86"
+CONFIGS="configs"
+ENVS=$(for c in $CONFIGS/*; do echo ${c#*/}; done)
+
+[ -z "$ENVS" ] && exit 1
 
 if [ "$1" == "compile" ]; then
 	for e in $ENVS; do 
@@ -19,18 +22,27 @@ if [ "$1" == "fastcompile" ]; then
 	done
 fi
 
-
-echo "UPLOADING FIRMWARE"
-
-if [ ! -z "$LOCAL_SERVER" ]; then
-	scp bin/ar71xx/openwrt-ar71xx-generic-tl-*sysupgrade*.bin $LOCAL_SERVER
-	scp bin/x86/openwrt-x86-alix2-combined-ext4.img.gz $LOCAL_SERVER
+if [ "$1" == "repository" ]; then
+	for e in $ENVS; do
+		echo "Preparing repository for $e"
+		sh set_environment.sh $e
+		make -j4
+	done
 fi
 
-if [ ! -z "$REMOTE_SERVER" ]; then
-	scp bin/ar71xx/openwrt-ar71xx-generic-tl-*sysupgrade*.bin $REMOTE_SERVER
-	scp bin/x86/openwrt-x86-alix2-combined-ext4.img.gz $REMOTE_SERVER
-fi
+if [ "$2" == "upload" ]; then
 
+	echo "UPLOADING FIRMWARE"
+
+	if [ ! -z "$LOCAL_SERVER" ]; then
+		scp bin/ar71xx/openwrt-ar71xx-generic-tl-*sysupgrade*.bin $LOCAL_SERVER
+		scp bin/x86/openwrt-x86-alix2-combined-ext4.img.gz $LOCAL_SERVER
+	fi
+	
+	if [ ! -z "$REMOTE_SERVER" ]; then
+		scp bin/ar71xx/openwrt-ar71xx-generic-tl-*sysupgrade*.bin $REMOTE_SERVER
+		scp bin/x86/openwrt-x86-alix2-combined-ext4.img.gz $REMOTE_SERVER
+	fi
+fi	
 
 
